88af0fa6417766b34ef5957f7e46a714b92fca94
Jan Gevaert <jan.gevaert@gmail.com>
2021-05-09 10:05:10 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/ParsingDateTime.java
@@ -0,0 +1,26 @@
+package edu.cnm.deepdive;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class ParsingDateTime {
+
+  public static void main(String[] args) {
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM dd yyyy");
+    LocalDate date = LocalDate.parse("02 15 2010", formatter);
+    LocalTime time = LocalTime.parse("11:33");
+
+    System.out.println(date);
+    System.out.println(time);
+
+    String text = date.format(formatter);
+    System.out.println();
+    System.out.println(text);
+    LocalDate parsedDate = LocalDate.parse(text, formatter);
+    System.out.println();
+    System.out.println(parsedDate);
+  }
+
+}
